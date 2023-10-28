aaa7bfc4957faeb29aba702bfdc6baf7ae7f799e
Vindalyn <vindalynne@outlook.com>
2023-10-29 00:19:54 +0200

    i hate searching for bugs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-const excavator_url = new URL('../pix/construction.gif', import.meta.url).href;
-
+//const excavator_url = new URL('../pix/construction.gif', import.meta.url).href;
+const test = "test";
 
 </script>
 <style>
